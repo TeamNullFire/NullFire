ee77cb0fd2235c2f13413c54e25aa00e66645027
rob_dcg_YT <75853142+coolegang@users.noreply.github.com>
2024-09-14 10:03:19 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,8 @@ local rep = "https://raw.githubusercontent.com/coolegang/NullHub/main/Games/"
 
 if game.PlaceId == 6839171747 then
     loadstring(game:HttpGet(rep.."Doors.lua"))()
+elseif game.PlaceId == 12552538292 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Fire-Hub/main/Core/Scripts/Pressure/Loader.lua")()
 else
     game.Players.LocalPlayer:Kick('Game is not added yet! Wait for balz')
 end
