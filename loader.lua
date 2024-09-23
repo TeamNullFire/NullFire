10789de346a7c5deb667fc2b4795b1a1715c6cb0
Infernus Scripts <139264639+InfernusScripts@users.noreply.github.com>
2024-09-23 20:55:32 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
---hi
+--[[hi
 
 local rep = "https://raw.githubusercontent.com/NuIlFire/NullFire/main/Games/"
 
@@ -10,3 +10,4 @@ elseif game.PlaceId == 12552538292 then
 else
     game.Players.LocalPlayer:Kick('Game is not added yet! Wait for balz')
 end
+]]
