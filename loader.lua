8bb05d50e0ff57c54ee219a7fb4b043f61cbabd8
seikoso <75853142+seikosos@users.noreply.github.com>
2025-04-03 14:33:23 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,4 @@ local rep = "https://raw.githubusercontent.com/NuIlFire/NullFire/main/Games/"
 
 if game.PlaceId == 6839171747 then
     loadstring(game:HttpGet(rep.."Doors.lua"))()
-else
-    game.Players.LocalPlayer:Kick('Game is not added yet! Wait for balz')
 end
