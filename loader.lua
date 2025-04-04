f6495328ea6aa9d280dac2c2649358b41931e521
seikoso <75853142+seikosos@users.noreply.github.com>
2025-04-04 20:13:04 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,6 @@
 
 local rep = "https://raw.githubusercontent.com/NuIlFire/NullFire/main/Games/"
 
-if game.PlaceId == 6839171747 then
+if table.find({87716067947993, 6839171747}, game.PlaceId) then
     loadstring(game:HttpGet(rep.."Doors.lua"))()
 end
