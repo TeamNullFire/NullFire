f64ea4a58e8836763859ae968fd932aa6d7ac54a
seikoso <75853142+seikosos@users.noreply.github.com>
2025-04-04 20:25:17 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,22 @@
 
 local rep = "https://raw.githubusercontent.com/NuIlFire/NullFire/main/Games/"
 
-if table.find({87716067947993, 6839171747}, game.PlaceId) then
-    loadstring(game:HttpGet(rep.."Doors.lua"))()
+local s, e = pcall(function()
+    return getgenv().request({
+    Url = "https://raw.githubusercontent.com/NuIlFire/NullFire/refs/heads/main/jhbfgs.json",
+    Method = "GET",
+    Body = nil,
+    Headers = {}
+})
+end)
+
+if s then
+    local data = e.Body
+    local dfnasdb = game:GetService("HttpService"):JsonDecode(data)
+
+    for i,v in dfnasdb do
+        if v.GameId == game.GameId then
+            loadstring(game:HttpGet(rep..i..".lua"))()
+        end
+    end
 end
