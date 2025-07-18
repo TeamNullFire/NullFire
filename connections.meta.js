0b24c1e2098c421ad068da86664242eb252c827c
seikoso <75853142+seikosos@users.noreply.github.com>
2025-07-18 15:35:59 +0200

    Create connections.meta.js

---
+++ connections.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        replace connections with friend
+// @namespace   nullfire
+// @match       https://*.roblox.com/*
+// @grant       none
+// @version     1.0.7
+// @author      seikoso
+// @description 18/07/2025, 15:10:26
+// @downloadURL https://github.com/TeamNullFire/NullFire/raw/refs/heads/main/connections.user.js
+// @updateURL   https://github.com/TeamNullFire/NullFire/raw/refs/heads/main/connections.meta.js
+// ==/UserScript==
